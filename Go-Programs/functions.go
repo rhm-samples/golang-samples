5dba9c5194c0e8a1943545900033bdbfcba505e0
vishal-kadam <33834843+vishal-kadam@users.noreply.github.com>
2020-05-27 17:05:52 +0530

    Update functions.go

--- Go-Programs/functions.go
+++ Go-Programs/functions.go
@@ -1,18 +1,17 @@
 package main
 import "fmt"
 
-func add(number1 int,number2 int) {
-
-    result := number1 + number2
-    fmt.Println(result)
-}
-
-
 func solve(number1 int,number2 int) (int,int) {
     
     sum := number1 + number2
     difference := number1 - number2
     return sum,difference
+}
+
+func add(number1 int,number2 int) {
+
+    result := number1 + number2
+    fmt.Println(result)
 }
 
 func main() {
@@ -21,7 +20,7 @@ func main() {
     b := 20
     add(a,b)
 
-	sum,difference := solve(a,b)
-	fmt.Printf("The sum of 2 numbers = %d\n",sum)
-	fmt.Printf("The difference between 2 numbers = %d\n",difference)
+    sum,difference := solve(a,b)
+    fmt.Printf("The sum of 2 numbers = %d\n",sum)
+    fmt.Printf("The difference between 2 numbers = %d\n",difference)
 }
