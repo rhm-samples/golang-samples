6baa0a2547e9583ea3c54e10c119276c6c39df2e
vishal-kadam <33834843+vishal-kadam@users.noreply.github.com>
2020-05-27 17:54:03 +0530

    Update maps.go

--- Go-Programs/maps.go
+++ Go-Programs/maps.go
@@ -3,23 +3,23 @@ import "fmt"
 
 func main() {
 
-	var mapNumberToWord = make(map[int]string)
-	mapNumberToWord[1]="one"
-	mapNumberToWord[2]="two"
-	mapNumberToWord[3]="three"
+    var mapNumberToWord = make(map[int]string)
+    mapNumberToWord[1]="one"
+    mapNumberToWord[2]="two"
+    mapNumberToWord[3]="three"
 
-	for key,value := range mapNumberToWord {
+    for key,value := range mapNumberToWord {
 
-		  fmt.Println(key,value)
-	}
+        fmt.Println(key,value)
+    }
 
-	//check if key is present
-	value,isPresent := mapNumberToWord[4]
-	if(isPresent == true) {
+    //check if key is present
+    value,isPresent := mapNumberToWord[4]
+    if(isPresent == true) {
 
-		fmt.Println("Word corresponding to 4 is - "+value)
-	}else {
+	fmt.Println("Word corresponding to 4 is - "+value)
+    }else {
 		
-		fmt.Println("4 is not present")
-	}
+	fmt.Println("4 is not present")
+    }
 }
