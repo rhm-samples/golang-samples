6771092a9048374df57379479daef2cb393bc5bd
vishal-kadam <33834843+vishal-kadam@users.noreply.github.com>
2020-05-27 17:04:40 +0530

    Create functions.go

---
+++ Go-Programs/functions.go
@@ -0,0 +1,27 @@
+package main
+import "fmt"
+
+func add(number1 int,number2 int) {
+
+    result := number1 + number2
+    fmt.Println(result)
+}
+
+
+func solve(number1 int,number2 int) (int,int) {
+    
+    sum := number1 + number2
+    difference := number1 - number2
+    return sum,difference
+}
+
+func main() {
+
+    a := 10
+    b := 20
+    add(a,b)
+
+	sum,difference := solve(a,b)
+	fmt.Printf("The sum of 2 numbers = %d\n",sum)
+	fmt.Printf("The difference between 2 numbers = %d\n",difference)
+}
