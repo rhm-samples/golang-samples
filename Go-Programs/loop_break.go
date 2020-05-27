098557f79f3731d61f767851fbb3814322fcdc71
vishal-kadam <33834843+vishal-kadam@users.noreply.github.com>
2020-05-27 17:48:29 +0530

    Update loop_break.go

--- Go-Programs/loop_break.go
+++ Go-Programs/loop_break.go
@@ -3,13 +3,13 @@ import "fmt"
 
 func main() {
 
-	var arr = [5]int {1,2,3,4,5}
-	for index := range arr {
+    var arr = [5]int {1,2,3,4,5}
+    for index := range arr {
 	
-		if(index == 3) {
+	if(index == 3) {
 			
-			break
-		}
-		fmt.Println(arr[index])
+            break
 	}
+	fmt.Println(arr[index])
+    }
 }
