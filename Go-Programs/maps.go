055b2726a523ceb723d89b8e9ff15992b1de52c8
vishal-kadam <33834843+vishal-kadam@users.noreply.github.com>
2020-05-27 17:25:58 +0530

    Create maps.go

---
+++ Go-Programs/maps.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+
+func main() {
+
+	var mapNumberToWord = make(map[int]string)
+	mapNumberToWord[1]="one"
+	mapNumberToWord[2]="two"
+	mapNumberToWord[3]="three"
+
+	for key,value := range mapNumberToWord {
+
+		  fmt.Println(key,value)
+	}
+
+	//check if key is present
+	value,isPresent := mapNumberToWord[4]
+	if(isPresent == true) {
+
+		fmt.Println("Word corresponding to 4 is-"+value)
+	}else {
+		
+		fmt.Println("4 is not present")
+	}
+}
