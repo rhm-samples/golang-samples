74473d3f6a4098e052a2b001a7f5ab053aee9938
vishal-kadam <33834843+vishal-kadam@users.noreply.github.com>
2020-05-27 17:52:02 +0530

    Update arrays.go

--- Go-Programs/arrays.go
+++ Go-Programs/arrays.go
@@ -3,11 +3,11 @@ import "fmt"
 
 func main() {
 
-	var arr = [5]int {1,2,3,4,5}
-	var sum int = 0
-	for i:=0;i<len(arr);i++ {
+    var arr = [5]int {1,2,3,4,5}
+    var sum int = 0
+    for i:=0;i<len(arr);i++ {
 		
-		  sum = sum + arr[i]
-	}
-	fmt.Println(sum)
+	sum = sum + arr[i]
+    }
+    fmt.Println(sum)
 }
